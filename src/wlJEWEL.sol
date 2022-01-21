9515cf55b9e11cacdc3c0837bef488dfc8230bc1
0xTomoyo <0xtomoyo@protonmail.com>
2022-01-21 13:23:13 +1100

    jewel broker

--- src/wlJEWEL.sol
+++ src/wlJEWEL.sol
@@ -7,7 +7,37 @@ import {IJewelToken} from "./interfaces/IJewelToken.sol";
 contract wlJEWEL is ERC20 {
     IJewelToken public immutable jewel;
 
+    mapping(address => JewelBroker) public brokers;
+
     constructor(address _jewel) ERC20("Wrapped Locked Jewels", "wlJEWEL", 18) {
         jewel = IJewelToken(_jewel);
+    }
+
+    function start(address account) external returns (JewelBroker broker) {
+        require(address(brokers[account]) == address(0), "cannot restart");
+        broker = new JewelBroker();
+        brokers[msg.sender] = broker;
+    }
+
+    function mint(address account) external returns (uint256 amount) {
+        amount = brokers[account].pull(account);
+        _mint(account, amount);
+    }
+}
+
+contract JewelBroker {
+    wlJEWEL public immutable wlJewel;
+    IJewelToken public immutable jewel;
+
+    constructor() {
+        wlJewel = wlJEWEL(msg.sender);
+        jewel = wlJEWEL(msg.sender).jewel();
+    }
+
+    function pull(address account) external returns (uint256 lock) {
+        require(msg.sender == address(wlJewel), "can only pull from wlJewel");
+        jewel.transfer(account, jewel.balanceOf(address(this)));
+        lock = jewel.lockOf(address(this));
+        jewel.transferAll(msg.sender);
     }
 }
