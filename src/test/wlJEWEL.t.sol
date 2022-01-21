a9af342364920cfcf6521398371aa0f4ec870e89
0xTomoyo <0xtomoyo@protonmail.com>
2022-01-21 15:31:46 +1100

    test:

--- src/test/wlJEWEL.t.sol
+++ src/test/wlJEWEL.t.sol
@@ -3,8 +3,18 @@ pragma solidity 0.8.10;
 
 import "ds-test/test.sol";
 
+import "./utils/Hevm.sol";
+import {IJewelToken} from "../interfaces/IJewelToken.sol";
+
 contract wlJEWELTest is DSTest {
-    function setUp() public {}
+    Hevm constant vm = Hevm(HEVM_ADDRESS);
+
+    IJewelToken constant JEWEL = IJewelToken(0x72Cb10C6bfA5624dD07Ef608027E366bd690048F);
+
+    function setUp() public {
+        vm.store(address(JEWEL), keccak256(abi.encode(address(this), 15)), bytes32(uint256(100_000 * 1e18)));
+        emit log_uint(JEWEL.totalSupply());
+    }
 
     function testExample() public {
         assertTrue(true);
